61caab3bdad82967b367505001366abb97892e69
Pat Cichocki <patrykcichocki@MBP-Patryk.connect>
2019-08-07 14:41:24 +0100

    Fade in effect in the intro section. Some graphical elements were also added.

---
+++ fadein.js
@@ -0,0 +1,5 @@
+function fadeIn(){
+    document.querySelector(".intro").classList.add("intro_displayed")
+    }
+
+window.onload = setTimeout(fadeIn, 1000);
--- scrolling.js
+++ scrolling.js
@@ -1,21 +1,22 @@
 // SCROLLING BETWEEN SECTIONS 
 
 function smoothScroll(target, time){
-    var target = document.querySelector(target);
-    var targetPos = target.getBoundingClientRect().top;
-    var startPos = window.pageYOffset;
-    var distance = targetPos - startPos;
-    var startTime = null;
+    var target = document.querySelector(target),
+        targetPos = target.getBoundingClientRect().top,
+        startPos = window.pageYOffset,
+        distance = targetPos - startPos,
+        startTime = null;
 
 
     function scrollAnimation(currentTime){ 
         if(startTime === null) startTime = currentTime;
-        var timeElapsed = currentTime - startTime;
-        var run = ease(timeElapsed, startPos, distance, time);
+        let timeElapsed = currentTime - startTime;
+        let run = ease(timeElapsed, startPos, distance, time);
         window.scrollTo(0, run);
         if(timeElapsed < time) requestAnimationFrame(scrollAnimation);
     }
 
+    //EASING FUNCTION 
     function ease(t, b, c, d) {
         return -c/2 * (Math.cos(Math.PI*t/d) - 1) + b;
       }
@@ -24,13 +25,15 @@ function smoothScroll(target, time){
 
 }
 
+// SECTIONS VARIABLES
 
-var nav_aboutme = document.querySelector('.nav_aboutme');
-var nav_journey = document.querySelector('.nav_journey');
-var nav_fc = document.querySelector('.nav_fc');
-var nav_contact = document.querySelector('.nav_contact');
+const nav_aboutme = document.querySelector('.nav_aboutme'),
+      nav_journey = document.querySelector('.nav_journey'),
+      nav_fc = document.querySelector('.nav_fc'),
+      nav_contact = document.querySelector('.nav_contact');
 
 
+// EVENT LISTENERS
 nav_aboutme.addEventListener('click', function() {
     smoothScroll('.aboutme_section', 500);
 });
